3c6a4e81b97f95c9ad72e3f2c8708c1d1ab7f829
Aron Zwaan <aronzwaan@gmail.com>
2022-10-04 14:55:45 +0000

    Fix invalid build script

--- build/run-compilation-benchmarks.sh
+++ build/run-compilation-benchmarks.sh
@@ -3,5 +3,4 @@
 set -eu
 
 now=$(date +%Y%m%dT%H%M%S)
-java -jar statix.benchmark.query.compilation-1.0.0-SNAPSHOT-jar-with-dependencies.jar ../sources/java-front/lang.java.statics
-| tee compilation-benchmark-$now.csv
+java -jar statix.benchmark.query.compilation-1.0.0-SNAPSHOT-jar-with-dependencies.jar ../sources/java-front/lang.java.statics | tee compilation-benchmark-$now.csv
