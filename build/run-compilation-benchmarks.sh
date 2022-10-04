4b881c2cbf9551c6fd2919421ddb13bf2572b9e1
Aron Zwaan <aronzwaan@gmail.com>
2022-10-04 14:52:40 +0200

    Update build scripts

--- build/run-compilation-benchmarks.sh
+++ build/run-compilation-benchmarks.sh
@@ -2,4 +2,6 @@
 
 set -eu
 
+now=$(date +%Y%m%dT%H%M%S)
 java -jar statix.benchmark.query.compilation-1.0.0-SNAPSHOT-jar-with-dependencies.jar ../sources/java-front/lang.java.statics
+| tee compilation-benchmark-$now.csv
--- build/test-query-correctness.sh
+++ build/test-query-correctness.sh
@@ -2,4 +2,5 @@
 
 set -eu
 
-java -cp statix.benchmark.builder-0.0.1-SNAPSHOT-jar-with-dependencies.jar mb.statix.benchmark.builder.QueryExtractor false commons-csv commons-io commons-lang3
+now=$(date +%Y%m%dT%H%M%S)
+java -cp statix.benchmark.builder-0.0.1-SNAPSHOT-jar-with-dependencies.jar mb.statix.benchmark.builder.QueryExtractor false commons-csv commons-io commons-lang3 | tee test-query-correctness-$now.log
