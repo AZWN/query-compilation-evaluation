161b3c3951196e35bee68d19ed99b48269a14bb6
Aron Zwaan <aronzwaan@gmail.com>
2022-10-06 11:42:51 +0200

    Add script for plotting micro benchmark results

---
+++ Rscripts/plot-micro-benchmarks.R
@@ -0,0 +1,64 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(ggplot2))
+
+files <- commandArgs(trailingOnly = TRUE)
+
+# Load data
+raw <- files[1] %>% read.csv(sep=";")
+compiled <- raw %>%
+  filter(Benchmark == "mb.statix.benchmark.resolution.ResolutionBenchmark.runCompiled") %>%
+  select(
+    Param..projectName_idx,
+    Score
+  ) %>%
+  rename(
+    ScoreCompiled = Score,
+    ProjectName_Index = Param..projectName_idx
+  )
+trad <- raw %>%
+  filter(Benchmark == "mb.statix.benchmark.resolution.ResolutionBenchmark.runTraditional") %>%
+  select(
+    Param..projectName_idx,
+    Score
+  ) %>%
+  rename(
+    ScoreTraditional = Score,
+    ProjectName_Index = Param..projectName_idx
+  )
+data <- inner_join(trad, compiled, "ProjectName_Index") %>%
+  mutate(
+    SpeedUp = ScoreCompiled / ScoreTraditional,
+    SpeedUpLog2 = log(SpeedUp, base = 2)
+  )
+
+# Plot Histogram
+pdf(
+  file = files[2],
+  width = 4,
+  height = 4
+)
+
+h <- hist(data$SpeedUpLog2,
+  breaks = 20,
+  freq = FALSE,
+
+  xlim = c(0, 12),
+  ylim = c(0, 0.5),
+
+  # Ensure axes connect properly
+  xaxs = "i",
+  yaxs = "i",
+
+  main = "Histogram of Speedup Factors",
+  xlab = expression(log[2](RT[gen] / RT[com])),
+  ylab = "Relative Frequency"
+)
+
+q <- quantile(data$SpeedUp)
+q <- append(q, q[4] - q[2])         # Calculate IQR (Q3 - Q1)
+q <- append(q, q[3] + 1.5 * q[6])   # Calculate Upper Fence (Q2 + 1.5IQR)
+q <- setNames(q, c("Min", "Q1 (25%)", "Mean (50%)", "Q3 (75%)", "Max", "IQR", "Upper Fence"))
+
+print(q)
